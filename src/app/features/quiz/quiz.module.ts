8c1e2e7e3f374cf17afd69af359871a99e6f3d4c
Zara Cooper <zramcooper@gmail.com>
2021-07-14 19:23:16 +0300

    Add question, quiz, and score pages

--- src/app/features/quiz/quiz.module.ts
+++ src/app/features/quiz/quiz.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuestionComponent } from './pages/question/question.component';
+import { QuizesComponent } from './pages/quizes/quizes.component';
+import { ScoreComponent } from './pages/score/score.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    QuestionComponent,
+    QuizesComponent,
+    ScoreComponent
+  ],
   imports: [
     CommonModule
   ]
