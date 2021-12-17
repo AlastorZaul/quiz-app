93dd53867fbc19c429823f5d8b6841bfbab83828
Zara Cooper <zramcooper@gmail.com>
2021-12-17 17:46:04 +0300

    Add user model Add methods to auth service Add storage service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+import { StorageService } from './storage.service';
+
+interface AuthResponse {
+  jwt: string;
+  user: User;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private url = `${environment.strapiUrl}/auth/local`;
 
-  constructor() { }
+  constructor(private http: HttpClient, private ss: StorageService) { }
+
+  login(identifier: string, password: string) {
+    return this.http.post<AuthResponse>(this.url, { identifier, password });
+  }
+
+  register(username: string, email: string, password: string) {
+    return this.http.post<AuthResponse>(`${this.url}/register`, { username, email, password });
+  }
+
+  isLoggedIn() {
+    return this.ss.getItem('loggedIn') === 'true';
+  }
 }
