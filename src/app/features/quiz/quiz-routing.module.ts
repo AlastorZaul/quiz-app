1b6b84d919a21b47dbeceac8d558a32795513430
Zara Cooper <zramcooper@gmail.com>
2021-12-17 18:01:54 +0300

    Add route guards Add signup route guard

--- src/app/features/quiz/quiz-routing.module.ts
+++ src/app/features/quiz/quiz-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedInGuard } from 'src/app/core/guards/logged-in.guard';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { QuizzesComponent } from './pages/quizzes/quizzes.component';
 import { ScoreComponent } from './pages/score/score.component';
+import { ScoresComponent } from './pages/scores/scores.component';
 
 const routes: Routes = [
     {
         path: '', component: QuizzesComponent
     },
     {
-        path: 'quiz/:id', component: QuizComponent
-    },
-    {
-        path: 'quiz/:id/score', component: ScoreComponent
+        path: '', canActivate: [LoggedInGuard], children: [
+            {
+                path: 'quiz/:id', component: QuizComponent
+            },
+            {
+                path: 'quiz/:id/score', component: ScoreComponent
+            },
+            {
+                path: '/score/:id', component: ScoreComponent
+            },
+            {
+                path: '/scores', component: ScoresComponent
+            }
+        ]
     }
 ];
 
