9a05df967c08b0f9f5a679fd4d81337d94342b2e
Zara Cooper <zramcooper@gmail.com>
2021-12-18 15:47:04 +0300

    Add auth header to score requests

--- src/app/data/services/score.service.ts
+++ src/app/data/services/score.service.ts
@@ -5,9 +5,7 @@ import { ScoreResponse } from '../models/score-response';
 import { Answer } from '../models/answer';
 import { Quiz } from '../models/quiz';
 import { Observable } from 'rxjs';
-import { StrapiResponse } from '../models/strapi-response';
-import { Score } from '../models/score';
-import { NormalizeService } from './normalize.service';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,19 +13,29 @@ import { NormalizeService } from './normalize.service';
 export class ScoreService {
   private url = `${environment.strapiUrl}/scores`;
 
-  constructor(private http: HttpClient, private ns: NormalizeService) { }
+  constructor(
+    private http: HttpClient,
+    private auth: AuthenticationService
+  ) { }
 
   createScore(quiz: Quiz, answers: Answer[]): Observable<ScoreResponse> {
-    return this.http.post<ScoreResponse>(this.url, { quiz, answers });
+    return this.http.post<ScoreResponse>(
+      this.url,
+      { quiz, answers },
+      this.auth.getAuthHeader()
+    );
   }
 
-  getScore(id: number): Observable<Score> {
-    return this.http.get<StrapiResponse>(`${this.url}/${id}`)
-      .pipe(this.ns.restructureData);
+  getScore(id: number): Observable<ScoreResponse> {
+    return this.http.get<ScoreResponse>(
+      `${this.url}/${id}`,
+      this.auth.getAuthHeader()
+    );
   }
 
-  getScores(): Observable<Score[]> {
-    return this.http.get<StrapiResponse>(this.url)
-      .pipe(this.ns.restructureArrayedAttributes());
+  getScores(): Observable<ScoreResponse[]> {
+    return this.http.get<ScoreResponse[]>(this.url,
+      this.auth.getAuthHeader()
+    );
   }
 }
