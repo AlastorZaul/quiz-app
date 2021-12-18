6d109a54a3b8d4fd20d5340975c7c5afec190e6b
Zara Cooper <zramcooper@gmail.com>
2021-12-18 15:45:54 +0300

    Remove strapi response model Remove unused methods from normalize service

--- src/app/data/services/normalize.service.ts
+++ src/app/data/services/normalize.service.ts
@@ -13,26 +13,26 @@ export class NormalizeService {
 
   constructor() { }
 
-  restructureData(source$: Observable<StrapiSingleEntryData>): Observable<any> {
-    return source$.pipe(map(v => v.data));
-  }
-
-  restructureAttributes(source$: Observable<StrapiSingleEntryData>): Observable<any> {
-    return source$.pipe(map(v => ({ id: v.data.id, ...v.data.attributes })));
+  restructureAttributes(nestedAttribute?: string): (source$: Observable<StrapiSingleEntryData>) => Observable<any> {
+    return source$ => source$.pipe(
+      map(v => this.restructureNestedAttributes(v.data, nestedAttribute)),
+    );
   }
 
   restructureArrayedAttributes(nestedAttribute?: string): (source$: Observable<StrapiMultiEntryData>) => Observable<any> {
     return source$ => source$.pipe(
       map(v => v.data),
       switchMap(v => v),
-      map(v => {
-        if (nestedAttribute) {
-          v.attributes[nestedAttribute] = v.attributes[nestedAttribute].data.map((nv: StrapiEntry) => ({ id: nv.id, ...nv.attributes }));
-        }
-
-        return { id: v.id, ...v.attributes };
-      }),
+      map(v => this.restructureNestedAttributes(v, nestedAttribute)),
       toArray()
     );
+  }
+
+  private restructureNestedAttributes(v: StrapiEntry, nestedAttribute?: string) {
+    if (nestedAttribute) {
+      v.attributes[nestedAttribute] = v.attributes[nestedAttribute].data.map((nv: StrapiEntry) => ({ id: nv.id, ...nv.attributes }));
+    }
+
+    return { id: v.id, ...v.attributes };
   }
 }
