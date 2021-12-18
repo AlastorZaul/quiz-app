587623779768342dad9eeede828e3641e41b4e67
Zara Cooper <zramcooper@gmail.com>
2021-12-18 15:46:47 +0300

    Populate questions when fetching quizzes

--- src/app/data/services/quiz.service.ts
+++ src/app/data/services/quiz.service.ts
@@ -1,28 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Quiz } from '../models/quiz';
-import { StrapiResponse } from '../models/strapi-response';
 import { NormalizeService } from './normalize.service';
+
+interface StrapiResponse {
+  data: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
   private url = `${environment.strapiUrl}/quizzes`;
+  private populateQuestionsParam = { params: new HttpParams().set('populate', '*') };
 
   constructor(private http: HttpClient, private ns: NormalizeService) { }
 
   getQuizzes(): Observable<Quiz[]> {
-    return this.http.get<StrapiResponse>(`${this.url}?populate=questions`)
+    return this.http.get<StrapiResponse>(
+      this.url,
+      this.populateQuestionsParam
+    )
       .pipe(this.ns.restructureArrayedAttributes('questions'));
   }
 
   getQuiz(id: number): Observable<Quiz> {
-    return this.http.get<StrapiResponse>(`${this.url}/${id}`)
+    return this.http.get<StrapiResponse>(`${this.url}/${id}`,
+      this.populateQuestionsParam
+    )
       .pipe(
-        this.ns.restructureAttributes
+        this.ns.restructureAttributes('questions')
       );
   }
 }
